b746e435d66abb40e121f1e68d34a1beae9f87f2
Sujal4920 <sujalsharma27may2000@gmail.com>
2023-07-05 14:49:35 +0530

    create header component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import SignUp from "./pages/SignUp.jsx";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Offers from "./pages/Offers.jsx";
 import './App.css';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <>
    <Router>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/profile" element={<Profile/>}/>
-      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/Sign-in" element={<SignIn/>}/>
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/forgot-password" element={<ForgotPassword/>}/>
       <Route path="/offers" element={<Offers/>}/>
